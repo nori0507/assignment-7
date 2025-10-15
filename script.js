e5a4e3f67009dc02a035509414d62871868a9aea
nori0507 <chikyu.rei@ic.edu>
2025-10-15 14:58:56 -0500

    completed step1 and 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
+let favoriteFoods = ["tacos", "ramen", "sushi", "cheese", "orange", "apple"]
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
-
-
+for (let i = 0; i<= favoriteFoods.length; i++) {
+  console.log("One of my favorite foods is " + favoriteFoods[i])
+}
 
 
 // 3. Print out the rating for each food with a ranking like:
